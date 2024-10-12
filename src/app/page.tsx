436d115401162adab0bc6b504a3e639cb51b0f50
evanletz <evanletzter@gmail.com>
2024-10-12 16:10:26 -0400

    Add links and svgs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="grid grid-cols-2 max-w-screen-md lg:max-w-screen-xl mx-auto">
-      <div className="border flex">
+      <div className="border flex flex-col">
         <Image
           src="/images/profile-photo.png"
           alt="Profile Photo"
@@ -11,7 +12,45 @@ export default function Home() {
           width={300}
           className="rounded-full"
         />
-        <p>Photo, name, tagline, links go here</p>
+        <h1>Evan Letz</h1>
+        <h2>An ideas guy that loves to build stuff.</h2>
+        <div id="links" className="flex gap-8">
+          <Link id="email" href={"mailto:evanletzter@gmail.com"}>
+            <img
+              src="/images/email.svg"
+              alt="Email Link"
+              width={25}
+              height={25}
+            />
+          </Link>
+          <Link id="linkedin" href={"https://www.linkedin.com/in/evan-letz/"}>
+            <img
+              src="/images/linkedin.svg"
+              alt="LinkedIn Link"
+              width={25}
+              height={25}
+            />
+          </Link>
+          <Link id="github" href={"https://github.com/evanletz/evanletz-site"}>
+            <img
+              src="/images/github.svg"
+              alt="Github Link"
+              width={25}
+              height={25}
+            />
+          </Link>
+          <Link id="x" href={"https://x.com/evanletz"}>
+            <img src="/images/x.svg" alt="X Link" width={25} height={25} />
+          </Link>
+          <Link id="resume" href={"#"}>
+            <img
+              src="/images/resume.svg"
+              alt="Resume Link"
+              width={25}
+              height={25}
+            />
+          </Link>
+        </div>
       </div>
       <div className="border flex">Skills, timeline go here</div>
     </div>
