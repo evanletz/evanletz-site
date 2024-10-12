c7fd102fd9dd72e1ebc93fa7a4e4456c13f2561b
evanletz <evanletzter@gmail.com>
2024-10-12 15:00:10 -0400

    Add rounded profile photo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="grid grid-cols-2 max-w-screen-md lg:max-w-screen-xl mx-auto">
+      <div className="border flex">
+        <Image
+          src="/images/profile-photo.png"
+          alt=""
+          height={300}
+          width={300}
+          className="rounded-full"
+        />
+        <p>Photo, name, tagline, links go here</p>
+      </div>
+      <div className="border flex">Skills, timeline go here</div>
+    </div>
+  );
 }
